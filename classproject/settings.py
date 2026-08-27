df2d924a0042eddc124217e75e5d51c7f421684c
Godsfavour123 <josephgodsfavour5@gmail.com>
2026-08-27 11:48:11 +0100

    Update

--- classproject/settings.py
+++ classproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,3 +120,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    }
+}
